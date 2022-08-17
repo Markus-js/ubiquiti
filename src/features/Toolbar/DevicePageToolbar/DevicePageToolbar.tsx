449fbb3240cc56fa45f54c5c8f1a0612dbb4ed6b
Markus-js <61780685+Markus-js@users.noreply.github.com>
2022-08-17 08:28:22 +0200

    toolbar for device page

---
+++ src/features/Toolbar/DevicePageToolbar/DevicePageToolbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useNavigate, useLocation } from 'react-router-dom';
+// REDUX
+import { useSelector } from "react-redux";
+// UTILS
+import { IDevice, IDeviceState } from '../../../utils/types';
+// ICONS
+import BackIcon from '../../../assets/back-icon.svg';
+
+const DevicePageToolbar = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [shortname] = (location.pathname.match(/([^/]*)$/) || []);
+  let device;
+  if (shortname) {
+    const devices = useSelector((state: IDeviceState) => state.devicesStore.devices)
+    device = devices?.find((device: IDevice) => device.shortnames[0] === shortname);
+  }
+
+  const handleNavigation = () => {
+    navigate('/')
+  }
+  return (
+    <>
+      <img onClick={() => handleNavigation()} src={BackIcon} alt="Back" />
+      <p>{device && device.line.name}</p>
+    </>
+  )
+}
+
+export default DevicePageToolbar
--- src/features/Toolbar/Toolbar.tsx
+++ src/features/Toolbar/Toolbar.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+// COMPONENTS
 import Filters from './Filter/Filter';
 import SearchBar from './SearchBar/SearchBar';
+import DevicePageToolbar from './DevicePageToolbar/DevicePageToolbar';
+// STYLE
 import './Toolbar.scss';
 
 const Toolbar = () => {
+  const location = useLocation();
+  const isDevicePage = location.pathname.includes('device');
+
   return (
-    <section className="toolbar">
-      <SearchBar />
-      <Filters />
+    <section className={`toolbar ${isDevicePage ? "device-page" : "home-page"}`}>
+      {isDevicePage
+        ?
+        <DevicePageToolbar />
+        :
+        <>
+          <SearchBar />
+          <Filters />
+        </>
+      }
     </section>
   )
 }
